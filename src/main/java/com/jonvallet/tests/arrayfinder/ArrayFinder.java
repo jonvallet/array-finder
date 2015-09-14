e5387a738f4bc86409e5d3796cde95861b3531e6
Jon Vallet <j.vallet@gmail.com>
2015-09-14 11:10:47 +0100

    Implement firstPostion method

--- src/main/java/com/jonvallet/tests/arrayfinder/ArrayFinder.java
+++ src/main/java/com/jonvallet/tests/arrayfinder/ArrayFinder.java
@@ -6,18 +6,27 @@ package com.jonvallet.tests.arrayfinder;
 public class ArrayFinder {
 
     /**
-     *  Will find the starting position of the second array in the
-     *  first array. i.e. [2,3,4,5] and [4,5] should return 2.
-     *
-     *  Index position will be 1 for first element.
-     *
-     *  0 will be returned if no element of the first array is found at the second array.
+     * Will find the starting position of the second array in the
+     * first array. i.e. [2,3,4,5] and [4,5] should return 2.
+     * <p>
+     * Index position will be 0 for first element.
+     * <p>
+     * -1 will be returned if no element of the second array is not found at the first array.
      *
      * @param firstArray
      * @param secondArray
      * @return
      */
     public static int firstPosition(int[] firstArray, int[] secondArray) {
-        return 0;
+
+        for (int i = 0; i < secondArray.length; i++) {
+            for (int j = 0; j < firstArray.length; j++) {
+                if (secondArray[i] == firstArray[j]) {
+                    return j;
+                }
+            }
+        }
+
+        return -1;
     }
 }

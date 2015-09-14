a1c31bc062f06c622de450f6103d12cc5fcb061a
Jon Vallet <j.vallet@gmail.com>
2015-09-14 10:14:47 +0100

    Initial commit wiht failing test

---
+++ src/test/java/com/jonvallet/tests/arrayfinder/ArrayFinderTest.java
@@ -0,0 +1,15 @@
+package com.jonvallet.tests.arrayfinder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jon on 14/09/15.
+ */
+public class ArrayFinderTest {
+
+    @Test
+    public void array2_3_4_5_and_4_5_returns_2() {
+        Assert.fail();
+    }
+}

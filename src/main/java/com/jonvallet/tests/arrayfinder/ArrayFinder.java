27fac4ec1b7277a8894ce9bdb79419729119e5f0
Jon Vallet <j.vallet@gmail.com>
2015-09-14 10:37:45 +0100

    Add skeleton implementation of ArrayFinder and finished test for it

---
+++ src/main/java/com/jonvallet/tests/arrayfinder/ArrayFinder.java
@@ -0,0 +1,23 @@
+package com.jonvallet.tests.arrayfinder;
+
+/**
+ * Created by jon on 14/09/15.
+ */
+public class ArrayFinder {
+
+    /**
+     *  Will find the starting position of the second array in the
+     *  first array. i.e. [2,3,4,5] and [4,5] should return 2.
+     *
+     *  Index position will be 1 for first element.
+     *
+     *  0 will be returned if no element of the first array is found at the second array.
+     *
+     * @param firstArray
+     * @param secondArray
+     * @return
+     */
+    public static int firstPosition(int[] firstArray, int[] secondArray) {
+        return 0;
+    }
+}
--- src/test/java/com/jonvallet/tests/arrayfinder/ArrayFinderTest.java
+++ src/test/java/com/jonvallet/tests/arrayfinder/ArrayFinderTest.java
@@ -10,6 +10,11 @@ public class ArrayFinderTest {
 
     @Test
     public void array2_3_4_5_and_4_5_returns_2() {
-        Assert.fail();
+        int firstArray [] = {2,3,4,5};
+        int secondArray [] = {4,5};
+
+        int expectedValue = 2;
+        int actualValue = ArrayFinder.firstPosition(firstArray, secondArray);
+        Assert.assertEquals(expectedValue, actualValue);
     }
 }
